c450680ad00904461cbf4d8e658ec8268657ed50
Cauê Lourenzo Batista <caue.batista.profissional@gmail.com>
2022-09-08 21:34:31 -0300

    Implementando DarkMode na tela de login

--- static/Login/codes/app.js
+++ static/Login/codes/app.js
@@ -1,6 +1,7 @@
 const sign_in_btn = document.querySelector('#sign-in-btn');
 const sign_up_btn = document.querySelector('#sign-up-btn');
 const container = document.querySelector('.container');
+const themeToggler = document.querySelector(".theme-toggler");
 
 sign_up_btn.addEventListener('click', () => {
     container.classList.add('sign-up-mode');
@@ -8,4 +9,9 @@ sign_up_btn.addEventListener('click', () => {
 
 sign_in_btn.addEventListener('click', () => {
     container.classList.remove('sign-up-mode');
+});
+
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables');
+    themeToggler.querySelector('span').classList.toggle('active');
 });
--- static/home/codes/app.js
+++ static/home/codes/app.js
@@ -6,15 +6,15 @@ const themeToggler = document.querySelector(".theme-toggler");
 // show sidebar
 menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block';
-})
+});
 
 // close sidebar
 closeBtn.addEventListener('click', () => {
     sideMenu.style.display = 'none';
-})
+});
 
 // change theme
 themeToggler.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme-variables');
     themeToggler.querySelector('span').classList.toggle('active');
-})
+});
