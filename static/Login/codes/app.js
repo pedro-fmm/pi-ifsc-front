ec11e28a38c8265e25950679df0d2d04b20f7c7e
Cauê Lourenzo Batista <caue.batista.profissional@gmail.com>
2022-09-11 20:02:44 -0300

    Dark mode tela de login funcionando

--- static/Login/codes/app.js
+++ static/Login/codes/app.js
@@ -1,7 +1,8 @@
 const sign_in_btn = document.querySelector('#sign-in-btn');
 const sign_up_btn = document.querySelector('#sign-up-btn');
 const container = document.querySelector('.container');
-const themeToggler = document.querySelector(".theme-toggler");
+const themeTogglerOne = document.querySelector(".theme-togglerOne");
+const themeTogglerTwo = document.querySelector(".theme-togglerTwo");
 
 sign_up_btn.addEventListener('click', () => {
     container.classList.add('sign-up-mode');
@@ -11,7 +12,12 @@ sign_in_btn.addEventListener('click', () => {
     container.classList.remove('sign-up-mode');
 });
 
-themeToggler.addEventListener('click', () => {
+themeTogglerOne.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme-variables');
-    themeToggler.querySelector('span').classList.toggle('active');
+    themeTogglerOne.querySelector('span').classList.toggle('active');
+});
+
+themeTogglerTwo.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables');
+    themeTogglerTwo.querySelector('span').classList.toggle('active');
 });
